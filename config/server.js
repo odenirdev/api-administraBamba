ae2047902d2464a083e33a4b07d58aee7c7449a9
Odenir <odenirdev@gmail.com>
2020-10-05 20:12:31 -0300

    commit initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c995c8bd0df136830a734fea1350e69'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10656208-077d-40a9-a74b-4df882c3c4fe'
+};
